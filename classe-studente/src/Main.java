4e090044fec4feb076a667654f412d5d9410c6db
39rahul <sk0679783@gmail.com>
2026-09-19 09:43:04 +0200

    utilizzo classe studente

---
+++ classe-studente/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        studente a, b;
+        a = new studente();
+        b = new studente();
+        a.nome = "Pietro";
+        a.cognome = "Rossi";
+        a.eta = 18;
+        a.peso = 70;
+        a.altezza = 1.80;
+        b.nome = "Giovanni";
+        b.cognome = "Bianchi";
+        b.eta = 17;
+        b.peso = 75;
+        b.altezza = 1.78;
+    }
+}
---
+++ classe-studente/src/studente.java
@@ -0,0 +1,7 @@
+public class studente {
+    String nome;
+    String cognome;
+    int eta;
+    float peso;
+    double altezza;
+}
