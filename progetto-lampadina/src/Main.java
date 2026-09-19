c5d9e8578f2acf2d8cef7294c49238a4f525522e
39rahul <sk0679783@gmail.com>
2026-09-19 09:27:39 +0200

    Utilizzo degli attributi

--- progetto-lampadina/src/Main.java
+++ progetto-lampadina/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Lampadina l;
+        Lampadina l, m;
         l = new Lampadina();
+        m = new Lampadina();
         l.potenza = 30;
+        m.potenza = 70;
    }
 }
