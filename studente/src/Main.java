6abb5e6b3fdf8078a8b8ff0bb1ff52aabdb03718
39rahul <sk0679783@gmail.com>
2026-09-22 12:50:30 +0200

    studente

---
+++ studente/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome, cognome;
+        int eta;
+        double altezza;
+
+        studente s = new studente ("Rahul", "Shibba", 17, 1.80);
+        System.out.println(s.nome);
+    }
+}
---
+++ studente/src/Studente.java
@@ -0,0 +1,16 @@
+
+    public class studente {
+        private String nome;
+        private String cognome;
+        private int eta;
+        private double altezza;
+
+        public studente(String name, String surname, int age, double height){
+            this.nome = name;
+            this.cognome = surname;
+            this.eta = age;
+            this.altezza = height;
+        }
+
+    }
+
