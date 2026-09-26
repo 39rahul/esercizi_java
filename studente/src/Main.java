691e71ca9f57efc660c322f5109f0501d9b3dbcc
39rahul <sk0679783@gmail.com>
2026-09-26 09:02:08 +0200

    esercizio studente e calcolo BMI

--- studente/src/Main.java
+++ studente/src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
+    static double calcolaIndice(Studente a){
+        double BMI = 0;
+        BMI = a.peso / (a.altezza * a.altezza);
+        return BMI;
+    }
     public static void main(String[] args) {
-        String nome, cognome;
-        int eta;
-        double altezza;
-
-        studente s = new studente ("Rahul", "Shibba", 17, 1.80);
-        System.out.println(s.nome);
+        Studente s;
+        double BMI = 0;
+        s = new Studente("Rahul", "Shibba", 17, 1.80, 75.);
+        System.out.printf("Lo studente " + s.cognome +" " + s.nome + " di "
+                + s.eta + " anni e alto " + s.altezza +
+                " m che pesa " + s.peso + " kg ");
+        BMI = calcolaIndice(s);
+        if(BMI < 18.5){
+            System.out.println(" è sottopeso e il suo indice corporeo è " + BMI);
+        }else if(BMI > 18.5 && BMI <=24.9){
+            System.out.println(" è normopeso e il suo indice corporeo è " + BMI);
+        }else if(BMI > 24.9 && BMI <= 29.9){
+            System.out.println(" è sovrappeso e il suo indice corporeo è "  + BMI);
+        }else{
+            System.out.println(" è obeso e il suo indice corporeo è " + BMI);
+        }
     }
 }
--- studente/src/Studente.java
+++ studente/src/Studente.java
@@ -1,16 +1,33 @@
+public class Studente {
+    String nome;
+    String cognome;
+    int eta;
+    double altezza;
+    double peso;
 
-    public class studente {
-        private String nome;
-        private String cognome;
-        private int eta;
-        private double altezza;
-
-        public studente(String name, String surname, int age, double height){
-            this.nome = name;
-            this.cognome = surname;
-            this.eta = age;
-            this.altezza = height;
+    public Studente(String nome, String cognome, int eta, double altezza, double peso){
+        this.nome = nome;
+        this.cognome=cognome;
+        this.eta=eta;
+        this.altezza=altezza;
+        this.peso = peso;
+        if(this.eta <= 0){
+            this.eta = 5;
         }
-
+        if(this.altezza <= 0){
+            this.altezza = 1.10;
+        }
+        if(this.peso <= 0){
+            peso = 30;
+        }
     }
 
+    public Studente(){
+        this.nome = "";
+        this.cognome = "";
+        this.eta = 0;
+        this.altezza = 0.0;
+        this.peso = 0.0;
+    }
+
+}
